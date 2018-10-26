e252e2de2b4b22a667b9a3951b9764405277f8e8
yizhoucao <yizhou.cao@finbook.co>
2018-10-26 13:51:31 +0800

    minor changes

--- src/client/SequenceClient.ts
+++ src/client/SequenceClient.ts
@@ -5,6 +5,15 @@ import util from '../utils/util';
 export default abstract class SequenceClient {
 	public sequenceWsClient: WebSocket | null = null;
 	public abstract handleSequenceMessage(m: string): any;
+
+	private reconnect(live: boolean) {
+		if (this.sequenceWsClient) {
+			this.sequenceWsClient.removeAllListeners();
+			this.sequenceWsClient.terminate();
+		}
+		this.connectToSequenceServer(live);
+	}
+
 	public connectToSequenceServer(live: boolean) {
 		this.sequenceWsClient = new WebSocket(
 			`${live ? CST.SEQUENCE_URL_LIVE : CST.SEQUENCE_URL_DEV}:${CST.SEQUENCE_PORT}`
@@ -14,19 +23,11 @@ export default abstract class SequenceClient {
 		this.sequenceWsClient.on('message', m => this.handleSequenceMessage(m.toString()));
 		this.sequenceWsClient.on('error', (error: Error) => {
 			util.logError(error);
-			if (this.sequenceWsClient) {
-				this.sequenceWsClient.removeAllListeners();
-				this.sequenceWsClient.terminate();
-			}
-			this.connectToSequenceServer(live);
+			this.reconnect(live);
 		});
 		this.sequenceWsClient.on('close', (code: number, reason: string) => {
 			util.logError('connection closed ' + code + ' ' + reason);
-			if (this.sequenceWsClient) {
-				this.sequenceWsClient.removeAllListeners();
-				this.sequenceWsClient.terminate();
-			}
-			this.connectToSequenceServer(live);
+			this.reconnect(live);
 		});
 	}
 }
