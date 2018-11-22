872c97abbfcf18383c90ebf028b29e7752550efc
WYANG008 <weitao.yang@finbook.co>
2018-11-22 17:15:39 +0800

    update bash

--- bash/orderProcessor.sh
+++ bash/orderProcessor.sh
@@ -1,4 +1,4 @@
 rm *.log
 killall -s KILL node
 npm run orders server $1 &> orders.log &
-npm run orderBooks server token=ZRX $1 &> orderBooks.log &
+npm run orderBooks server $1 &> orderBooks.log &
