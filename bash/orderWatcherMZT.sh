f8660ef6dae0af9cfda6b6acdf28e936a1b0d76a
WYANG008 <weitao.yang@finbook.co>
2018-12-13 20:22:27 +0800

    update bash

--- bash/orderWatcherMZT.sh
+++ bash/orderWatcherMZT.sh
@@ -1,3 +1,3 @@
 rm *.log
 killall -s KILL node
-npm run orderWatcher server token=sETH,LETH,sETH-M19,LETH-M19 $1 &> orderWatcher-all.log &
+npm run orderWatcher server tokens=sETH,LETH,sETH-M19,LETH-M19 $1 &> orderWatcher-all.log &
