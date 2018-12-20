1a0187a392133297ef51a1f3ee87adf9925b33da
peiru.li@finbook.co <peiru.li@finbook.co>
2018-12-20 15:54:14 +0800

    update serverMaster test

--- src/server/serverMasterUtil.test.ts
+++ src/server/serverMasterUtil.test.ts
@@ -1,4 +1,5 @@
 import child_process from 'child_process';
+import osUtil from '../utils/osUtil';
 import util from '../utils/util';
 import serverMasterUtil from './serverMasterUtil';
 
@@ -43,7 +44,8 @@ test('retry within short time', () => {
 	expect(serverMasterUtil.subProcesses['token']).toMatchSnapshot();
 });
 
-test('launchTokenPair fail', () => {
+test('launchTokenPair fail win32', () => {
+	osUtil.isWindows = jest.fn(() => true);
 	child_process.exec = jest.fn() as any;
 	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
 	serverMasterUtil.retry = jest.fn();
@@ -62,7 +64,8 @@ test('launchTokenPair fail', () => {
 	expect((serverMasterUtil.retry as jest.Mock<void>).mock.calls).toMatchSnapshot();
 });
 
-test('launchTokenPair success', () => {
+test('launchTokenPair success win 32', () => {
+	osUtil.isWindows = jest.fn(() => true);
 	child_process.exec = jest.fn(() => {
 		return {
 			on: jest.fn()
@@ -87,7 +90,8 @@ test('launchTokenPair success', () => {
 	).toMatchSnapshot();
 });
 
-test('launchTokenPair debug', () => {
+test('launchTokenPair debug win32', () => {
+	osUtil.isWindows = jest.fn(() => true);
 	child_process.exec = jest.fn() as any;
 	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
 	serverMasterUtil.subProcesses['token'] = {
@@ -104,20 +108,65 @@ test('launchTokenPair debug', () => {
 	expect(serverMasterUtil.subProcesses).toMatchSnapshot();
 });
 
-// test('launchTokenPair live', () => {
-// 	child_process.exec = jest.fn() as any;
-// 	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
-// 	serverMasterUtil.subProcesses['token'] = {
-// 		token: 'token',
-// 		lastFailTimestamp: 0,
-// 		failCount: 0,
-// 		instance: undefined as any
-// 	};
-// 	serverMasterUtil.launchTokenPair('tool', 'token', {
-// 		forceREST: false,
-// 		debug: false,
-// 		live: true
-// 	} as any);
-// 	expect(((child_process.exec as any) as jest.Mock<Promise<void>>).mock.calls).toMatchSnapshot();
-// 	expect(serverMasterUtil.subProcesses).toMatchSnapshot();
-// });
+test('launchTokenPair fail not win32', () => {
+	osUtil.isWindows = jest.fn(() => false);
+	child_process.exec = jest.fn() as any;
+	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
+	serverMasterUtil.retry = jest.fn();
+	serverMasterUtil.subProcesses['token'] = {
+		token: 'token',
+		lastFailTimestamp: 0,
+		failCount: 0,
+		instance: undefined as any
+	};
+	serverMasterUtil.launchTokenPair('tool', 'token', {
+		debug: false,
+		live: false
+	} as any);
+	expect(((child_process.exec as any) as jest.Mock<Promise<void>>).mock.calls).toMatchSnapshot();
+	expect(serverMasterUtil.subProcesses).toMatchSnapshot();
+	expect((serverMasterUtil.retry as jest.Mock<void>).mock.calls).toMatchSnapshot();
+});
+
+test('launchTokenPair success not win32', () => {
+	osUtil.isWindows = jest.fn(() => false);
+	child_process.exec = jest.fn(() => {
+		return {
+			on: jest.fn()
+		};
+	}) as any;
+	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
+	serverMasterUtil.retry = jest.fn();
+	serverMasterUtil.subProcesses['token'] = {
+		token: 'token',
+		lastFailTimestamp: 5,
+		failCount: 0,
+		instance: undefined as any
+	};
+	serverMasterUtil.launchTokenPair('tool', 'token',  {
+		debug: false,
+		live: false
+	} as any);
+	expect(((child_process.exec as any) as jest.Mock<Promise<void>>).mock.calls).toMatchSnapshot();
+	expect(serverMasterUtil.subProcesses).toMatchSnapshot();
+	expect(
+		(serverMasterUtil.subProcesses['token'].instance.on as jest.Mock<void>).mock.calls).toMatchSnapshot()
+});
+
+test('launchTkenPair debug not win32', () => {
+	osUtil.isWindows = jest.fn(() => false);
+	child_process.exec = jest.fn() as any;
+	util.getUTCNowTimestamp = jest.fn(() => 1234567890);
+	serverMasterUtil.subProcesses['token'] = {
+		token: 'token',
+		lastFailTimestamp: 0,
+		failCount: 0,
+		instance: undefined as any
+	};
+	serverMasterUtil.launchTokenPair('tool', 'token',  {
+		debug: true,
+		live: false
+	} as any);
+	expect(((child_process.exec as any) as jest.Mock<Promise<void>>).mock.calls).toMatchSnapshot();
+	expect(serverMasterUtil.subProcesses).toMatchSnapshot();
+});
--- src/utils/osUtil.ts
+++ src/utils/osUtil.ts
@@ -4,6 +4,10 @@ class OsUtil {
 	public getHostName() {
 		return os.hostname();
 	}
+
+	public isWindows() {
+		return process.platform === 'win32';
+	}
 }
 
 const osUtil = new OsUtil();
