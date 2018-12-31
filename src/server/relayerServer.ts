8d83e860843f728e8f5b0b0f3aad84a0a1817b7a
yizhoucao <yizhou.cao@finbook.co>
2018-12-31 18:23:00 +0800

    minor changes

--- src/server/relayerServer.ts
+++ src/server/relayerServer.ts
@@ -118,8 +118,7 @@ class RelayerServer {
 				else this.sendErrorOrderResponse(ws, req, req.orderHash, CST.WS_INVALID_ORDER);
 			} else {
 				util.logDebug('invalid orderHash, ignore');
-				util.logDebug(orderHash);
-				this.sendErrorOrderResponse(ws, req, req.orderHash, CST.WS_INVALID_ORDER);
+				this.sendErrorOrderResponse(ws, req, req.orderHash, orderHash);
 			}
 		} catch (error) {
 			util.logError(error);
