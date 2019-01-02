b1ad8cdc2d6da47f66030538cfb0a7b7dc4e70ce
WYANG008 <weitao.yang@finbook.co>
2019-01-02 18:39:08 +0800

    update

--- src/client/marketMaker.ts
+++ src/client/marketMaker.ts
@@ -160,7 +160,7 @@ class MarketMaker {
 			await web3Util.awaitTransactionSuccessAsync(tx);
 			this.tokenBalances[2] -= bTokenToCreate;
 			this.tokenBalances[1] -= bTokenToCreate * alpha;
-			util.logDebug(`wrapping ether`);
+			util.logDebug(`wrapping ether with amt ${ethAmountForRedemption}`);
 			await util.sleep(10000);
 			tx = await web3Util.wrapEther(ethAmountForRedemption, this.makerAccount.address);
 			util.logDebug(`tx hash: ${tx}`);
