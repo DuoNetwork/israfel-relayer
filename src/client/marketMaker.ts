aed649b2f76882dc88344ed68db4fa58cf7d8136
WYANG008 <weitao.yang@finbook.co>
2019-01-08 11:52:55 +0800

    minor update

--- src/client/marketMaker.ts
+++ src/client/marketMaker.ts
@@ -325,10 +325,10 @@ class MarketMaker {
 			? otherTokenOrderBook.asks[0].price
 			: Number.MAX_VALUE;
 
-		util.logInfo(
+		util.logDebug(
 			`[${otherPair}] no arb bid ${otherTokenNoArbBidPrice} vs best bid ${otherTokenBestBid}`
 		);
-		util.logInfo(
+		util.logDebug(
 			`[${otherPair}] no arb ask ${otherTokenNoArbAskPrice} vs best ask ${otherTokenBestAsk}`
 		);
 		const orderHashesToCancel: string[] = [];
