ecb3407d722cc6956f4de5509cc6e000d1abeb99
yizhoucao <yizhou.cao@finbook.co>
2019-01-18 10:19:34 +0800

    update osUtil test

--- src/utils/osUtil.test.ts
+++ src/utils/osUtil.test.ts
@@ -1,14 +1,9 @@
 import osUtil from './osUtil';
 
 jest.mock('os', () => ({
-	hostname: () => 'hostname',
-	platform: 'win32'
+	hostname: () => 'hostname'
 }));
 
 test('getHostName', () => {
 	expect(osUtil.getHostName()).toBe('hostname');
-});
-
-test('isWindows', () => {
-	expect(osUtil.isWindows()).toBeTruthy();
 });
