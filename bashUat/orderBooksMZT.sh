56e6c67bdf20f70ae0ba0f9657969c24cf38d909
yizhoucao <yizhou.cao@finbook.co>
2019-02-21 13:47:55 +0800

    update package

--- bashUat/orderBooksMZT.sh
+++ bashUat/orderBooksMZT.sh
@@ -1,3 +1,4 @@
 rm *.log
 killall -s KILL node
 npm run orderBooks tokens=sETH,LETH,sETH-M19,LETH-M19 server env=uat $1 &> orderBooks-all.log &
+npm run orderBooks tokens=ETH-100P-3H server env=uat $1 &> orderBooks.ETH-100P-3H.log &
