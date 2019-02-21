7503f917a27aed820387ad9c7985bfbbcbbd2b5c
WYANG008 <weitao.yang@finbook.co>
2019-02-21 11:02:23 +0800

    update bash

--- bash/orderWatcher.sh
+++ bash/orderWatcher.sh
@@ -1,4 +1,4 @@
 rm *.log
 killall -s KILL node
 npm run node server $1 &> node.log &
-npm run orderWatcher server tokens=aETH,bETH $1 &> orderWatcher-btv.log &
+npm run orderWatcher server tokens=ETH-100C-3H,ETH-100P-3H $1 &> orderWatcher-vvd.log &
