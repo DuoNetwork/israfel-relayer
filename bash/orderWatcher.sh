be4b91d18fb70a83ab655b0e90a161733f13b21f
yizhoucao <yizhou.cao@finbook.co>
2019-02-21 13:52:29 +0800

    update dev script

--- bash/orderWatcher.sh
+++ bash/orderWatcher.sh
@@ -1,4 +1,4 @@
 rm *.log
 killall -s KILL node
 npm run node server $1 &> node.log &
-npm run orderWatcher server tokens=aETH,bETH $1 &> orderWatcher-all.log &
+npm run orderWatcher server tokens=aETH,bETH $1 &> orderWatcher-btv.log &
