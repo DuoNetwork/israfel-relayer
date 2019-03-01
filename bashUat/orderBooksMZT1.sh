3cf84226cf58fa2c8e3f6950d9ff193b67294194
WYANG008 <weitao.yang@finbook.co>
2019-03-01 12:45:54 +0800

    update bash

--- bashUat/orderBooksMZT1.sh
+++
@@ -1,3 +0,0 @@
-rm *.log
-killall -s KILL node
-npm run orderBooks tokens=sETH,LETH,ETH-100P-3H server env=uat $1 &> orderBooks-all.log &
---
+++ bashUat/orderBooksVVD.sh
@@ -0,0 +1,1 @@
+npm run orderBooks tokens=ETH-100P-3H,ETH-100C-3H server env=uat $1 &> orderBooks.ETH-100C-3H.log &
